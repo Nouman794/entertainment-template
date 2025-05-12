973942b6383875ad3c4876ba30c66ec5572a969f
Nouman Akhtar <nouman.akhtar@starzplayarabia.com>
2025-05-12 13:59:46 +0500

    Initial project setup with Fat free frameWork

---
+++ vendor/composer/installed.php
@@ -0,0 +1,41 @@
+<?php return array(
+    'root' => array(
+        'name' => 'nouman/entertainment-template',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'ab7b66426fe684bd20be0d69ad5642eb9b2c0092',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'bcosca/fatfree' => array(
+            'pretty_version' => '3.9.0',
+            'version' => '3.9.0.0',
+            'reference' => '9563e848a403c8e932633f8daef7dda5b2f64ec6',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../bcosca/fatfree',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'bcosca/fatfree-core' => array(
+            'pretty_version' => '3.9.0',
+            'version' => '3.9.0.0',
+            'reference' => '227b3b03315ab86b1601d9e9fd8e07efbedf6ab9',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../bcosca/fatfree-core',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+        'nouman/entertainment-template' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'ab7b66426fe684bd20be0d69ad5642eb9b2c0092',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
